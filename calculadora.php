aa6018c46a288831feb9405fc08e6a67f97ba1f0
Mr-Mr-r <101223477+Mr-Mr-r@users.noreply.github.com>
2022-04-01 22:22:15 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -1,28 +1,28 @@
 <?php
 
-$Valor=$_POST["Valor"];
-$Valor2=$_POST["Valor2"];
+$Valor1=$_POST["first_number"];
+$Valor2=$_POST["secund_number"];
 $operacao=$_POST["operacao"];
-
+$resultado;
 
     switch($operacao){ 
       case "soma" :
-                $resultado=$Valor+$Valor2;
+                $resultado=$Valor1+$Valor2;
                 echo "A soma é: ".$resultado;
         break;
 
         case "sub" :
-                $resultado=$Valor-$Valor2;
+                $resultado=$Valor1-$Valor2;
                 echo "A subtraçao é: ".$resultado;
         break;
 
         case "divisao" :
-                $resultado=$Valor/$Valor2;
+                $resultado=$Valor1/$Valor2;
                 echo "A divisão é: ".$resultado;
         break;
         
         case "mult" :
-                $resultado=$Valor*$Valor2;
+                $resultado=$Valor1*$Valor2;
                 echo "A multiplicação é: ".$resultado;
         break;
         
